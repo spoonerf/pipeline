31773d909b56b9104a84523644df7195081dce78
spoonerf <spoonerfiona@gmail.com>
2019-12-17 16:04:24 +0000

    code to create multiple files for the cluster. Want to submit ~170 jobs so using this to change job name and some directories etc

---
+++ batch_job_file_maker.R
@@ -0,0 +1,61 @@
+
+files <- list.files(here::here("species_lists/"))
+
+if(!dir.exists(here::here("r_jobs"))){
+  dir.create(here::here("r_jobs"))
+}
+               
+           
+for(file in files){
+  
+  ptc<-readLines(here::here("r_jobs", "job_001.R"))  #Read in the template file
+  job <- gsub(".csv", "", file)
+  ptc[17] <- gsub("job_001", job, ptc[17])
+  ptc[46] <- gsub("job_001.csv", file, ptc[46])
+  file_out <- gsub(".csv", ".R", file)
+  writeLines(ptc, here::here("r_jobs",file_out))  #Use this as the file name and write it to the output directory
+  
+}
+
+
+###R file to delete files created during job - after it has been tarred up.
+
+if(!dir.exists(here::here("delete_jobs"))){
+  dir.create(here::here("delete_jobs"))
+}
+
+
+for(file in files){
+  
+  ptc<-readLines(here::here("delete_jobs", "job_001.R"))  #Read in the template file
+  job <- gsub(".csv", "", file)
+  ptc[1] <- gsub("job_001", job, ptc[1])
+  ptc[3] <- gsub("job_001.csv", file, ptc[3])
+  file_out <- gsub(".csv", ".R", file)
+  writeLines(ptc, here::here("delete_jobs",file_out))  #Use this as the file name and write it to the output directory
+  
+}
+
+
+####bash_scripts to submit all the jobs
+
+if(!dir.exists(here::here("bash_scripts"))){
+  dir.create(here::here("bash_scripts"))
+}
+
+files <- gsub("\\..*","",files)
+
+for(file in files){
+  
+  ptc <- readLines("job_001.sh")  #Read in the template file
+  ptc[5] <- gsub("job_001", file, ptc[5])
+  ptc[23] <- gsub("job_001", file, ptc[23])
+  ptc[24] <- gsub("job_001", file, ptc[24])
+  ptc[25] <- gsub("job_001", file, ptc[25])
+  ptc[26] <- "\n"
+  writeLines(ptc, here::here("bash_scripts",paste0(file, ".sh")))  #Use this as the file name and write it to the output directory
+  
+}
+
+
+
